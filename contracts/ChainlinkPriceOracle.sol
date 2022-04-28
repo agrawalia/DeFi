f86ec61cfe541b578a1704f08baa18047e4e43cf
Ishan Agrawal <agrawalia@rknec.edu>
2022-04-28 08:43:25 +0530

    Chainlink Price oracle
    ETH/USD

---
+++ contracts/ChainlinkPriceOracle.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract ChainlinkPriceOracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor(){
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);  //Price of ETH in USD      
+    }
+
+    function getLatestPrice() public view returns(int) {
+        (   uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price / 1e8;
+    }
+}
